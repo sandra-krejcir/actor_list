db61541d6fb4599e1971c4e22cb10afea68a9ad9
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-08-25 21:01:57 +0200

    font&color change, adding margins&boxes

--- script.js
+++ script.js
@@ -30,8 +30,32 @@ function showProduct(actor) {
   theActor.textContent = actor.fullname;
   theActor.setAttribute("id", actor._id);
   copy.querySelector(".details").setAttribute("id", `${actor._id}popup`);
-  copy.querySelector(".moName").textContent = `Starrs in: ${actor.movie}`;
+  copy.querySelector(".moName").textContent = `Starrs in: "${actor.movie}"`;
   copy.querySelector(".exit").setAttribute("id", `${actor._id}X`);
+  copy.getElementById(actor._id).addEventListener("click", PopUp);
+  const popUp = copy.getElementById(`${actor._id}popup`);
+  const exClose = copy.getElementById(`${actor._id}X`);
+  function PopUp() {
+    console.log("function PopUp");
+    popUp.classList.remove("disapear");
+    exClose.addEventListener("click", popClose);
+  }
+  function popClose() {
+    console.log("function popClose()");
+    popUp.classList.add("disapear");
+  }
+  if (actor.movie == "Pulp Fiction") {
+    copy.querySelector("h2").classList.add("purple");
+  }
+  if (actor.movie == "Goodfellas") {
+    copy.querySelector("h2").classList.add("blue");
+  }
+  if (actor.movie == "Fight Club") {
+    copy.querySelector("h2").classList.add("red");
+  }
+  if (actor.movie == "Inception") {
+    copy.querySelector("h2").classList.add("yellow");
+  }
   const parent = document.querySelector("ul");
   parent.appendChild(copy);
 }
