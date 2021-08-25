a303fefe12700dd474e16c4d58d168346f15dd29
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-08-25 09:46:11 +0200

    hopefully fetched the json file

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 const URL = "https://kea21s-6eb0.restdb.io/rest/actorlist";
+
+load();
+
+function load() {
+    fetch(URL, {
+      method: "GET",
+      headers: {
+        "x-apikey": "606d606af55350043100752e",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        listActors(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  
+  function listActors(data) {
+    data.forEach(showActor);
+  }
+  
+  function showActor(actor) {
+    console.log(actor);
+    const template = document.querySelector(".theList").content;
+    const copy = template.cloneNode(true);
+    copy.querySelector(".actor_name") = actor.fullname;
+    copy.querySelector(".details").setAttribute('id','1');
+    copy.querySelector(".movie_name") = actor.movie;
+    copy.querySelector(".exit").setAttribute('id','exit1');
+    const parent = document.querySelector("ul");
+  parent.appendChild(copy);
+  }
