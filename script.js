74b5dce6aae987da99627ecbce3d3440c47115af
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-08-25 10:04:34 +0200

    finallly fetched- DON'T FORGET .textContent!

--- script.js
+++ script.js
@@ -3,33 +3,33 @@ const URL = "https://kea21s-6eb0.restdb.io/rest/actorlist";
 load();
 
 function load() {
-    fetch(URL, {
-      method: "GET",
-      headers: {
-        "x-apikey": "606d606af55350043100752e",
-      },
+  fetch(URL, {
+    method: "GET",
+    headers: {
+      "x-apikey": "606d606af55350043100752e",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      listProducts(data);
     })
-      .then((res) => res.json())
-      .then((data) => {
-        listActors(data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
-  
-  function listActors(data) {
-    data.forEach(showActor);
-  }
-  
-  function showActor(actor) {
-    console.log(actor);
-    const template = document.querySelector(".theList").content;
-    const copy = template.cloneNode(true);
-    copy.querySelector(".actor_name") = actor.fullname;
-    copy.querySelector(".details").setAttribute('id','1');
-    copy.querySelector(".movie_name") = actor.movie;
-    copy.querySelector(".exit").setAttribute('id','exit1');
-    const parent = document.querySelector("ul");
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+function listProducts(data) {
+  data.forEach(showProduct);
+}
+
+function showProduct(actor) {
+  console.log(actor);
+  const template = document.querySelector(".theList").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector(".acName").textContent = actor.fullname;
+  copy.querySelector(".details").setAttribute("id", "1");
+  copy.querySelector(".moName").textContent = actor.movie;
+  copy.querySelector(".exit").setAttribute("id", "exit1");
+  const parent = document.querySelector("ul");
   parent.appendChild(copy);
-  }
+}
